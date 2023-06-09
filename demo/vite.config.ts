db6f084e85a736a8e29c2abf1425e4f7fc3fcb45
cdefg666 <791150351@qq.com>
2023-06-09 13:57:19 +0800

    chore: remove unused configs

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -6,24 +6,15 @@ export default defineConfig({
   resolve: {
     dedupe: ["react", "react-dom"],
     alias: {
-      web3: "web3/dist/web3.min.js",
       // buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6", // add buffer
     },
   },
   build: {
     target: "es2020",
     sourcemap: true,
-    rollupOptions: {
-      external: ["@lit-protocol/sdk-nodejs"],
-    },
   },
   server: {
     port: 5173,
     host: "0.0.0.0",
-  },
-  define: {
-    "process.env": {
-      ENV: "Browser",
-    },
   },
 });
